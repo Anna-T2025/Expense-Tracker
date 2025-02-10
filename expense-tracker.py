50b7a44cda34e80da949a557c8722332f047e5c8
Emory107 <emorysun07@gmail.com>
2025-02-10 11:59:21 -0800

    Update expense-tracker.py

--- expense-tracker.py
+++ expense-tracker.py
@@ -6,20 +6,25 @@ total_expenses = []
 def add_expense():
     amount = input("Enter your expense:\n")
     date_str = input("Enter the reminder date (YYYY-MM-DD):\n")
-    purpose = input("Where did you spend this money on:\n")
+    Category = input("Where did you spend this money on:\n")
     try:
         amount = float(amount)
         date = datetime.strptime(date_str, '%Y-%m-%d').date()
-        expense = {"Purpose" : purpose, "amount": amount, "date":date}
+        expense = {"Category" : category, "amount": amount, "date":date}
         total_expenses.append(expense)
         print ("A new expense haas been added")
         return total_expenses
     except ValueError:
         print("Invalid input. Please enter a valid amount and date (YYYY-MM-DD).")
 
-def sort_expense_by_date():
-   # Use sorted() function to sort expense by date
-   return sorted(total_expenses, key=lambda total_expenses: datetime.strptime(expense['date'], '%Y-%m-%d'))
+def filter_expenses_by_category(category):
+   filtered = [expense for expense in expenses if expense["category"] == category]
+   if not filtered:
+       print(f"No expenses found for category '{category}'.")
+       return
+   print(f"\nExpenses for category '{category}':")
+   for i, expense in enumerate(filtered, 1):
+       print(f"{i}. Amount: ${expense['amount']}, Date: {expense['date']}")
 
 def view_expense():
     if no total_expenses:
@@ -27,9 +32,9 @@ def view_expense():
         return
     sorted_expense = sort_expense_by_date()
     for index,expense in enumerate (sorted_expense):
-        print (f"{index+1}. {date} - {purpose}: ${amount:.2f}")
+        print (f"{index+1}. {date} - {category}: ${amount:.2f}")
         
-def calculate_total():
+def calculate_total_expense():
     total = sum (total_expenses["amount"] for expense in total_expenses)
     print (f"Total expenses: ${total:.2f}")
 
@@ -40,7 +45,7 @@ def delete_expense():
         if 0 <= index < len(total_expenses):
            # Remove the selected reminder
            removed = sort_expense_by_date(total_expenses).pop(index)
-           print(f"Removed expense: {removed['date']} - {removed['purpose']}: {removed[amount]}")
+           print(f"Removed expense: {removed['date']} - {removed['category']}: {removed[amount]}")
         else:
            # Handle invalid index
            print("Invalid number. Please try again.")
